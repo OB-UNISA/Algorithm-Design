c21118bdcd32f984a3cef268f9dee61d3cee71e8
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2022-03-10 22:47:50 +0100

    made the test of the algorithm with dynamic values, they change at every run

--- lessons/contapromossi.py
+++ lessons/contapromossi.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def contapromossi(a, k, n, i, j):
     if i == j:
         if k < a[i]:
@@ -10,7 +13,17 @@ def contapromossi(a, k, n, i, j):
     return contapromossi(a, k, n, i, m)
 
 
-array = [10, 15, 16, 17, 18, 19, 20, 25, 27, 30]
-len_array = len(array)
+array = [randint(0, 30) for _ in range(randint(5, 20))]
+array.sort()
+_n = len(array)
+_k = randint(0, 30)
 
-print(contapromossi(array, 17, len_array, 0, len_array - 1))
+alg_res = contapromossi(array, _k, _n, 0, _n - 1)
+
+expected_value = 0
+for ind, el in enumerate(array):
+	if el > _k:
+		expected_value = _n - ind
+		break
+
+print(f'array: {array}\nk: {_k}\nexpected result: {expected_value}\nalg result: {alg_res}\nsame results: {expected_value == alg_res}')
