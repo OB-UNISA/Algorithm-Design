0f9f2b3baa8a32487a588d5fb4cf6e26b4d63d14
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2022-03-29 09:26:27 +0200

    fixed a bug when sometimes the generated array would have all values negative, but the precondition is that a[n-1] > 0

--- exercises/exercises2_10.py
+++ exercises/exercises2_10.py
@@ -12,9 +12,7 @@ def alg(a, i, j):
             return "non c'è"
 
     m = (i + j) // 2
-    if a[m] < 0 and a[m + 1] > 0:
-        return m
-    elif a[m] > 0:
+    if a[m] > 0:
         return alg(a, i, m)
     else:
         return alg(a, m, j)
@@ -26,9 +24,11 @@ for _ in range(LIM // 2):
     array = [randint(LIM * -1, LIM) for _ in (range(randint(1, LIM)))]
     array.sort()
     _n = len(array)
+    if array[_n - 1] < 0:
+        array.append(array[_n - 1] * -1 + 1)
 
     alg_res = alg(array, 0, _n - 1)
-
+    
     expected_value = "non c'è"
     for ind in range(_n - 1):
         if array[ind] < 0 and array[ind + 1] > 0:
