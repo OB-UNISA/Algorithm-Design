4cc769461b970fc5d70ebe5753e3aaa52ef5ccaa
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2022-04-01 03:57:52 +0200

    bug fixes

--- exercises/exercises2_4.py
+++ exercises/exercises2_4.py
@@ -60,17 +60,16 @@ def alg(a, i, j, L, U):
 
 LIM = 15
 for k in range(LIM // 2):
-    array = [-15, -10, -5, -4, -3, 0, 4, 15]#[randint(LIM * -1, LIM) for _ in (range(randint(1, LIM)))]
+    array = [randint(LIM * -1, LIM) for _ in (range(randint(1, LIM)))]
     array.sort()
     array = list(dict.fromkeys(array))
     _n = len(array)
     if randint(0, 3) == 1:
-        _L = -15#randint(LIM * -1, max(array))
+        _L = randint(LIM * -1, max(array))
     else:
-        _L = -15#randint(LIM * -1, LIM)
-    _U = 15#randint(_L + 1, LIM)
+        _L = randint(LIM * -1, LIM)
+    _U = randint(_L + 1, LIM  + LIM // 5)
 
-    print(f'Array: {array}\n L: {_L}\n U: {_U}')
     alg_res = alg(array, 0, _n - 1, _L, _U)
 
     expected_value = 0
